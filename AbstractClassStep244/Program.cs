1701f4cb5c64bad802b299aeefadd9b7b4849410
student <student@learncodinganywhere.com>
2020-12-14 16:50:29 -0800

    Add project files.

---
+++ AbstractClassStep244/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassStep244
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee individual = new Employee();
+
+            individual.firstName = "Sample";
+            individual.lastName = "Student";
+            individual.SayName();
+            Console.ReadLine();
+        }
+    }
+}
